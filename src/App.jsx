dd515cae2d0edd7d7850844ac65397c0a752aa01
Amit Roy <amitroy497@yahoo.com>
2024-06-15 22:35:31 +0530

    feat(basic-data-fetching-using-usequery): basic data fetching using usequery

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
+import { fetchPosts } from './api/api';
 
 function App() {
+	const { data, isLoading, isError, error, status } = useQuery({
+		queryKey: ['posts'],
+		queryFn: fetchPosts,
+	});
+
+	console.log(data, isLoading, status);
+
 	return <>Hello World!</>;
 }
 
