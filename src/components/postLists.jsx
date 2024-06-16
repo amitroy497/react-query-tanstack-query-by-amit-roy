d6ed85260de0ea1339d5609783f98577b36aadcb
Amit Roy <amitroy497@yahoo.com>
2024-06-16 13:26:01 +0530

    feat(post-usemutation): post method uing usemutation and use of usequeryclient

--- src/components/postLists.jsx
+++ src/components/postLists.jsx
@@ -1,8 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { fetchPosts } from '../api/api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { addPost, fetchPosts, fetchTags } from '../api/api';
 
 export const PostLists = () => {
+	const queryCLient = useQueryClient();
+
 	const {
 		data: postData,
 		isError,
@@ -12,10 +13,80 @@ export const PostLists = () => {
 		queryKey: ['posts'],
 		queryFn: fetchPosts,
 	});
+
+	const { data: tagsData } = useQuery({
+		queryKey: ['tags'],
+		queryFn: fetchTags,
+	});
+
+	const {
+		mutate,
+		isError: isPostError,
+		isPending,
+		error: postError,
+		reset,
+	} = useMutation({
+		mutationFn: addPost,
+		onMutate: () => {
+			return { id: 1 };
+		}, // runs before mutate function
+		onSuccess: (data, variables, context) => {
+			queryCLient.invalidateQueries({
+				queryKey: ['posts'],
+				exact: true,
+				// predicate: () => {
+				// 	query.queryKey[0] === 'posts' && query.quertKey[1].page >= 2;
+				// },
+			});
+		}, // runs after mutate function
+		// onError: (error, variables, context) => {},
+		// onSettled: (data, error, variables, context) => {},
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const title = formData.get('title');
+		const tags = Array.from(formData.keys()).filter(
+			(key) => formData.get(key) === 'on'
+		);
+
+		if (!title || !tags) return;
+
+		mutate({ id: postData.length + 1, title, tags });
+
+		e.target.reset();
+	};
+
 	return (
 		<div className='container'>
-			{isLoading && <p>Loadding ...</p>}
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Enter your post..'
+					className='postbox'
+					name='title'
+				/>
+				<div className='tags'>
+					{tagsData?.map((tag) => {
+						return (
+							<div key={tag}>
+								<input name={tag} id={tag} type='checkbox' />
+								<label htmlFor={tag}>{tag}</label>
+							</div>
+						);
+					})}
+				</div>
+				<button>Post</button>
+			</form>
+			{isLoading && isPending && <p>Loadding ...</p>}
 			{isError && <p>{error?.message}</p>}
+			{isPostError && (
+				<>
+					<p>{postError?.message}</p>
+					<button onClick={() => reset()}>Reset</button>
+				</>
+			)}
 
 			{postData?.map((post) => {
 				return (
